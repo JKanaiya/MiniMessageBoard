75fc0736efd99256c4a14dbf1a1fb4f9c744fce2
jonathan <jonathankanaiya@gmail.com>
2025-06-05 15:29:46 +0300

    feat: Completed Project Assignment

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const app = express();
+const path = require("node:path");
 
-app.listen(3000, () => {})
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {});
---
+++ routes/indexRouter.js
@@ -0,0 +1,68 @@
+const { Router } = require("express");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hi there!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+let open = "";
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) =>
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+  }),
+);
+
+indexRouter.get("/new", (req, res) =>
+  res.render("form", {
+    title: "New Message Form",
+  }),
+);
+
+indexRouter.get("/open/:text/:user/:added", (req, res) => {
+  res.render("open", {
+    text: req.params.text,
+    user: req.params.user,
+    added: req.params.added,
+  });
+});
+
+indexRouter.get("/messages", (req, res) => {
+  console.log(open);
+  res.render("messages", {
+    text: open.text,
+    user: open.user,
+    added: open.added,
+  });
+});
+
+indexRouter.post("/messages", (req, res) => {
+  console.log(req.params);
+  open = {
+    text: req.body.messageContent,
+    user: req.body.userName,
+    added: req.body.added,
+  };
+  res.redirect("/messages");
+});
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageContent,
+    user: req.body.userName,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+module.exports = indexRouter;
