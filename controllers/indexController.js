203683f26290febe7af6988c295ee69281c0c8d1
jonathan <jonathankanaiya@gmail.com>
2025-06-12 12:32:45 +0300

    fix: corrected missing title

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,7 @@ const db = require("../db/queries");
 async function getAllMessages(req, res) {
   const messages = await db.getAllMessages();
   res.render("index", {
+    title: "MiniMessageBoard",
     messages: messages,
   });
 }
